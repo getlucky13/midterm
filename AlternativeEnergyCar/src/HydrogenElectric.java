54a8601fa7355a575ddb83c7cd0f133b598e8e5f
Ron Rizzo <churchmouth13@gmail.com>
2023-03-29 12:55:06 -0400

    Remove storageFormat field and references
    Due to restructuring, storageFormat field is redundant and thus was
    removed.

--- AlternativeEnergyCar/src/HydrogenElectric.java
+++ AlternativeEnergyCar/src/HydrogenElectric.java
@@ -1,27 +1,16 @@
 public class HydrogenElectric extends ElectricMotor {
-    String engineType; 
     int storageCapacity;
 
     public HydrogenElectric() {
-        engineType = "NA";
         storageCapacity = 0;
     }
 
     public HydrogenElectric(int year, int doors, int power, double price, String manufacturer, String model, String country,
-            String style, String layout, boolean inProduction, int maxRange, String engineType,
-            int storageCapacity) {
+            String style, String layout, boolean inProduction, int maxRange, int storageCapacity) {
         super(year, doors, power, price, manufacturer, model, country, style, layout, inProduction, maxRange);
-        this.engineType = engineType;
         this.storageCapacity = storageCapacity;
     }
 
-    public String getEngineType() {
-        return engineType;
-    }
-
-    public void setEngineType(String engineType) {
-        this.engineType = engineType;
-    }
 
     public int getStorageCapacity() {
         return storageCapacity;
@@ -32,14 +21,13 @@ public class HydrogenElectric extends ElectricMotor {
     }
 
     public String capacityToString() {
-        return storageCapacity + " kWh";
+        return storageCapacity + " kWh battery. ";
     }
     
     @Override
     public String toString() {
         return "Hydrogen Fuel Cell Car: " + this.carNameToString() + " | " + doors + " door " 
-        + layout + " " + style + ", " + power + " horsepower. " + "Engine Type: " + engineType +
-        "Max range of " + this.maxRange + " miles."
+        + layout + " " + style + ", " + power + " horsepower. " + capacityToString() + "Max range of " + this.maxRange + " miles."
         +". " + "Product of " + country + ". " + this.inProductionToString() + " " + formattedPrice();
     }
 }
