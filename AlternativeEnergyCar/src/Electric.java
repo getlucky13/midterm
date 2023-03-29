34909ff7e18c2d1c5a975020bff1156e5c2ce3ce
Ron Rizzo <churchmouth13@gmail.com>
2023-03-29 12:13:36 -0400

    Create Hydrogen.java and overide displayInfo
    Creates Hydrogen.java and adds an overide to Electric.java for the
    displayInfo method

--- AlternativeEnergyCar/src/Electric.java
+++ AlternativeEnergyCar/src/Electric.java
@@ -1,4 +1,6 @@
 public class Electric extends AEC{
+
+
     int maxRange;
 
     public Electric() {
@@ -22,7 +24,18 @@ public class Electric extends AEC{
     @Override
     public String toString() {
         return "Electric Car: " + this.carNameToString() + " | " + doors + " door " 
-        + layout + " " + style + ", " + power + " horsepower. " + "Max range of " + this.maxRange
+        + layout + " " + style + ", " + power + " horsepower. " + "Max range of " + this.maxRange + " miles."
         +". " + "Product of " + country + ". " + this.inProductionToString() + " " + formattedPrice();
+    }    
+    
+    @Override
+    public void displayInfo() {
+        System.out.println("Alternative Energy Car");
+        System.out.println(this.carNameToString());
+        System.out.println(formattedPrice() + " new");
+        System.out.println(doors + " door " + layout + " " + style);
+        System.out.println("Max range: " + maxRange + " miles");
+        System.out.println("Product of " + country);
+        System.out.println(this.inProductionToString());
     }
 }
