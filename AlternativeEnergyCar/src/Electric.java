ddfb1ee9bf764e00ea9a463ca7e18ce4fa26ed81
Ron Rizzo <churchmouth13@gmail.com>
2023-03-29 12:01:09 -0400

    Adds code for Electric.java
    Adds majority of code for Electric.java functionality.

---
+++ AlternativeEnergyCar/src/Electric.java
@@ -0,0 +1,28 @@
+public class Electric extends AEC{
+    int maxRange;
+
+    public Electric() {
+            
+    }
+
+    public Electric(int[] efficiency, int year, int doors, int power, double price, String manufacturer, String model,
+            String country, String style, String layout, boolean inProduction, int maxRange) {
+        super(efficiency, year, doors, power, price, manufacturer, model, country, style, layout, inProduction);
+        this.maxRange = maxRange;
+    }
+
+    public int getMaxRange() {
+        return maxRange;
+    }
+
+    public void setMaxRange(int maxRange) {
+        this.maxRange = maxRange;
+    }
+    
+    @Override
+    public String toString() {
+        return "Electric Car: " + this.carNameToString() + " | " + doors + " door " 
+        + layout + " " + style + ", " + power + " horsepower. " + "Max range of " + this.maxRange
+        +". " + "Product of " + country + ". " + this.inProductionToString() + " " + formattedPrice();
+    }
+}
