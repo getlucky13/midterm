5658790180146d9f03384e544837365cc81bb012
Ron Rizzo <churchmouth13@gmail.com>
2023-03-30 11:23:53 -0400

    More testing code added
    Created three AEC objects from info stored in child objects. Adds code
    to demonstrate polymorphic nature of AEC class and children. Reorganized
    and formatted output for easier understanding

--- AlternativeEnergyCar/src/Driver.java
+++ AlternativeEnergyCar/src/Driver.java
@@ -18,20 +18,46 @@ public class Driver {
         "Brazil", "Hatchback", "FWD", false, 1.3, 4, 100, 
         false, true, false, 0, 18);
 
-        ArrayList<AEC> blarg = new ArrayList<>();
-        blarg.add(aptera);
-        blarg.add(rx8);
-        blarg.add(mirai);
-        blarg.add(fiat147);
 
-        for(AEC car: blarg) {
+        AEC miraiAEC = new AEC(mirai.year, mirai.doors, mirai.power, mirai.price, mirai.manufacturer, mirai.model, 
+        mirai.country, mirai.style, mirai.layout, mirai.inProduction);
+
+        AEC apteraAEC = new AEC(aptera.year, aptera.doors, aptera.power, aptera.price, aptera.manufacturer, 
+        aptera.model, aptera.country, aptera.style, aptera.layout, aptera.inProduction);
+
+        AEC rx8AEC = new AEC(rx8.year, rx8.doors, rx8.power, rx8.price, rx8.manufacturer, rx8.model, rx8.country, 
+        rx8.style, rx8.layout, rx8.isInProduction());
+
+        ArrayList<AEC> polymorphicTest = new ArrayList<>();
+        polymorphicTest.add(rx8AEC);
+        polymorphicTest.add(apteraAEC);
+        polymorphicTest.add(miraiAEC);
+
+        System.out.println("---Alternative Energy Cars: polymorphicTest---");
+        for(AEC car: polymorphicTest) {
             car.displayInfo();
+            System.out.println();
         }
 
-        Collections.sort(blarg);
-
-        for(AEC car: blarg){
+        ArrayList<AEC> comparableTest = new ArrayList<>();
+        comparableTest.add(aptera);
+        comparableTest.add(rx8);
+        comparableTest.add(mirai);
+        comparableTest.add(fiat147);
+        
+        System.out.println("---Alternative Energy Cars: comparableTest Unsorted---");
+        for(AEC car: comparableTest) {
             car.displayInfo();
+            System.out.println();
+        }
+
+        Collections.sort(comparableTest);
+
+        System.out.println("---Alternative Energy Cars: comparableTest Sorted by Price---");
+        for(AEC car: comparableTest){
+            System.out.println(car.carNameToString());
+            System.out.println(car.formattedPrice());
+            System.out.println();
         }
     }
 }
